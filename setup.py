53f0712467446d0a676236d6a3a57c455e3ac89b
codenonprofits <codenonprofits@users.noreply.github.com>
2017-09-14 00:32:55 -0300

    Add lib requirements

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+CLASSIFIERS = [
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development',
+]
+
+setup(
+    name='coordinates-extractor',
+    version='0.0.1',
+    author='CodeNonprofits - GreatNonprofits',
+    author_email='support@greatnonprofits.org',
+    packages=['coordinates-extractor'],
+    url='https://github.com/codenonprofits/coordinates-extractor',
+    license='BSD licence, see LICENCE.txt',
+    description='Library consultation phone carriers around the world.',
+    download_url="https://github.com/codenonprofits/coordinates-extractor/archive/master.zip",
+    keywords=['coordinates', 'extractor', 'google maps', 'maps', 'python'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'beautifulsoup4==4.6.0',
+        'cffi==1.10.0',
+        'chardet==3.0.4',
+        'idna==2.6',
+        'pycparser==2.18',
+        'requests[security]',
+        'six==1.10.0',
+        'urllib3==1.22'
+    ]
+)

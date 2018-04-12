6ab05d6e8e557f27c4ed6e12ee43014a6ee17498
codenonprofits <codenonprofits@users.noreply.github.com>
2018-04-12 13:57:37 -0300

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 
 setup(
     name='coordinates-extractor',
-    version='0.0.2',
+    version='0.0.3',
     author='CodeNonprofits - GreatNonprofits',
     author_email='support@greatnonprofits.org',
     packages=['coordinates_extractor'],
